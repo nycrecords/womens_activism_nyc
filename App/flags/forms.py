f8bfb302ff6e9a3cc2ea69f6c8e326a72b0aec1a
btang <blt264@nyu.edu>
2016-08-01 15:43:43 -0400

    Edited FlagsForm

--- App/flags/forms.py
+++ App/flags/forms.py
@@ -8,7 +8,8 @@ flag_choices = [('Inappropriate Content', 'Inappropriate Content'), ('Offensive 
 
 
 class FlagsForm(Form):
-    flag_reason = SelectField('Reason for flagging?', choices=flag_choices, validators=[DataRequired('Please Select a reason')])
-    flag_description = TextAreaField('Please provide a brief description', validators=[Length(0, 500)])
+    flag_reason = SelectField('Please choose a reason for flagging:', choices=flag_choices,
+                              validators=[DataRequired('Please select a reason.')])
+    flag_description = TextAreaField('Please provide a brief description:', validators=[Length(0, 500)])
     recaptcha = RecaptchaField()
     submit = SubmitField('Submit')

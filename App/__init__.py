940d84097ddc4031c893a997fef144ebce72256a
btang <blt264@nyu.edu>
2016-08-01 17:03:00 -0400

    Fixing merge conflicts

--- App/__init__.py
+++ App/__init__.py
@@ -2,33 +2,24 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
-<<<<<<< .merge_file_7Nsj9Z
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
-from flask_pagedown import PageDown
-=======
-from flask.ext.sqlalchemy import SQLAlchemy
-from config import config
->>>>>>> .merge_file_BFDQmf
+
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
-<<<<<<< .merge_file_7Nsj9Z
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
-pagedown = PageDown()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     config[config_name].init_app(app)
 
@@ -37,7 +28,6 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -58,21 +48,3 @@ def create_app(config_name):
     app.register_blueprint(posts_blueprint)
 
     return app
-=======
-
-def create_app(config_name):
-    current_app = Flask(__name__)
-    current_app.config.from_object(config[config_name])
-    config[config_name].init_app(current_app)
-
-    bootstrap.init_app(current_app)
-    mail.init_app(current_app)
-    moment.init_app(current_app)
-    db.init_app(current_app)
-
-    from .main import main as main_blueprint
-    current_app.register_blueprint(main_blueprint)
-
-    return current_app
-
->>>>>>> .merge_file_BFDQmf

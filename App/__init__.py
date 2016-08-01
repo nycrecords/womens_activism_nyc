b4ff770dd58ae70ca8dd06796da952f37c605581
btang <blt264@nyu.edu>
2016-08-01 17:15:00 -0400

    merge again

--- App/__init__.py
+++ App/__init__.py
@@ -2,22 +2,15 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
-<<<<<<< .merge_file_Sgedsv
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
-
-=======
-from flask.ext.sqlalchemy import SQLAlchemy
-from config import config
->>>>>>> .merge_file_QNMNbo
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
-<<<<<<< .merge_file_Sgedsv
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -54,21 +47,3 @@ def create_app(config_name):
     app.register_blueprint(posts_blueprint)
 
     return app
-=======
-
-def create_app(config_name):
-    current_app = Flask(__name__)
-    current_app.config.from_object(config[config_name])
-    config[config_name].init_app(current_app)
-
-    bootstrap.init_app(current_app)
-    mail.init_app(current_app)
-    moment.init_app(current_app)
-    db.init_app(current_app)
-
-    from .main import main as main_blueprint
-    current_app.register_blueprint(main_blueprint)
-
-    return current_app
-
->>>>>>> .merge_file_QNMNbo
--- App/main/__init__.py
+++ App/main/__init__.py
@@ -2,8 +2,4 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-<<<<<<< .merge_file_fd57nL
 from . import main, views
-=======
-from . import main,views
->>>>>>> .merge_file_ThjgUw

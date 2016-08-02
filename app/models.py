9639bafb4d1fa576afe3716f21ddd561f66658f2
Joel Castillo <jcastillo@records.nyc.gov>
2016-08-02 09:08:56 -0400

    Updating backref name for user_role relationship

--- app/models.py
+++ app/models.py
@@ -154,7 +154,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+    users = db.relationship('User', backref='user_role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name

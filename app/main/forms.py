d1997e85764f333e6cc7e81387726be21dca5c17
sgong <sgong@records.nyc.gov>
2016-08-02 15:06:33 -0400

    deleting is finished editing needs to have CK editor

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import Form 
-from wtforms import StringField, SubmitField 
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired 
 from flask_wtf.recaptcha import RecaptchaField
 
 
 class PostForm(Form):
     title = StringField('Title', validators=[DataRequired('Please enter a title')])
-    content = PageDownField("What's on your mind?", validators=[DataRequired("Body can't be left blank")])
+    content = TextAreaField("What's on your mind?", validators=[DataRequired("Body can't be left blank")])
     recaptcha = RecaptchaField()
+    submit = SubmitField('submit')
+
+
+class DeleteForm(Form):
     submit = SubmitField('submit')
--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template, redirect, url_for, current_app, flash, reques
 from .. import db
 from ..models import *
 from . import main
+from .forms import PostForm, DeleteForm
+import bleach
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -40,3 +42,69 @@ def index(data=None):
     #posts = Post.query.order_by(Post.creation_time.desc()).all()
     return render_template('index.html', posts=posts, pagination=pagination)
 
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = Post.query.get_or_404(id)
+    # if current_user != post.author and \
+    #         not current_user.can(Permission.ADMINISTER):
+    #     abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        # post.body = form.body.data
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.add(post)
+        db.session.commit()
+        flash('The post has been updated.')
+        return redirect(url_for('main.index'))
+    post = Post.query.filter_by(id=id).first()
+    form.title.data = post.title
+    form.content.data = strip_html(post.content)
+    return render_template('edit_post.html', form=form)
+
+
+@main.route('/delete/post/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+    """
+    Return the confirmation page for deleting a post.
+
+    Keyword arguments:
+    id -- the post id
+    """
+    post = Post.query.get_or_404(id)
+    # if current_user != post.author and not current_user.can(Permission.ADMINISTER) and not current_user.is_director():
+    #     abort(403)
+    form = DeleteForm()
+    if form.validate_on_submit():
+        post = Post.query.filter_by(id=id).first()
+        post.is_visible=False
+        # posttags = PostTag.query.filter_by(post_id=id).all()
+        # for posttag in posttags:
+        #     db.session.delete(posttag)
+        # comments = Comment.query.filter_by(post_id=id).all()
+        # for comment in comments:
+        #     db.session.delete(comment)
+        # db.session.delete(post)
+        db.session.commit()
+        flash('The post has been deleted.')
+        return redirect(url_for('.index'))
+    return render_template('delete_post.html', form=form)
+
+
+def strip_html(html_str):
+    """
+    a wrapper for bleach.clean() that strips ALL tags from the input
+    :param html_str: string that needs to be stripped
+    :return: a bleached string
+    """
+    tags = []
+    attr = {}
+    styles = []
+    strip = True
+
+    return bleach.clean(html_str,
+                       tags=tags,
+                       attributes=attr,
+                       styles=styles,
+                       strip=strip)

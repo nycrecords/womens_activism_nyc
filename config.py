a7caf0f3e179200086d4417641da7cf907c14f9a
sgong <sgong@records.nyc.gov>
2016-08-08 13:35:23 -0400

    html files for blocks

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or None
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None
     WOMENS_MAIL_SUBJECT_PREFIX = '[Women\'s Activism NYC]'
-    WOMENS_MAIL_SENDER = 'Women\'s Activism NYC Admin <flasky@example.com>'
-    WOMENS_ADMIN = 'womensactivismnyc@gmail.com'
+    MAIL_SENDER = "Women's Activism <sgong@records.nyc.gov>"
+    MAIL_ADMIN = "Women's Activism <womensactivismnyc@gmail.com>"
     RECAPTCHA_PUBLIC_KEY = '6LetUSYTAAAAALgTT8Rt2nLZ2OTBNph6Qa1TbrAH'
     RECAPTCHA_PRIVATE_KEY = '6LetUSgYTAAAAAN1pLPLyyIFUh6rEemXczfMFT4um'
     POSTS_PER_PAGE = 10

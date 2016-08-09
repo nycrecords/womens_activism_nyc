b800413f8167e15c0793c4490218ad7be354881b
Jimmo <jmo@records.nyc.gov>
2016-08-09 12:32:47 -0400

    Fixed relative imports in app/db_helpers.py; app/send_email.py

--- app/send_email.py
+++ app/send_email.py
@@ -1,7 +1,7 @@
 from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
-from . import mail
+from app import mail
 
 
 def send_async_email(app, msg):

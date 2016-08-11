598d2d25a1c9baac059c31160a666b68c998704e
btang <blt264@nyu.edu>
2016-08-11 11:07:15 -0400

    Hardcoded test tags with checkboxes and checked for value in views

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,17 +1,15 @@
 from flask_wtf import Form 
-from wtforms import StringField, SubmitField 
-from wtforms.validators import DataRequired 
-from flask_wtf.recaptcha import RecaptchaField
-
-
-class PostForm(Form):
-    title = StringField('Title', validators=[DataRequired('Please enter a title')])
-    content = PageDownField("What's on your mind?", validators=[DataRequired("Body can't be left blank")])
-    recaptcha = RecaptchaField()
-    submit = SubmitField('submit')
-
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+from ..models import Tag
 
 class Comment(Form):
     content = StringField('Comment', validators=[DataRequired("Comment can't be left blank")])
     submit = SubmitField('submit')
 
+
+class TagForm(Form):
+    # for tag in Tag.query.all():
+    #     tag.name = BooleanField(tag.name)
+    tag1 = BooleanField('tag1')
+    tag2 = BooleanField('tag2')
--- app/main/views.py
+++ app/main/views.py
@@ -3,10 +3,16 @@ from .. import db
 from ..models import *
 from . import main
 from .. import recaptcha
+from .forms import TagForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index(data=None):
+
+    form = TagForm()
+
+    tag1 = form.tag1.data
+    print (tag1)
 
     page = request.args.get('page', 1, type=int)
     pagination = Post.query.order_by(Post.creation_time.desc()).paginate(
@@ -19,17 +25,17 @@ def index(data=None):
     if data or request.method == 'POST':
         if request.form['input_title'] == '':
             if request.form['editor1'] == '':
-                return render_template('index.html', posts=posts, pagination=pagination, tags=tags)
+                return render_template('index.html', posts=posts, pagination=pagination, tags=tags, form=form)
             else:
                 flash('Please enter a title.')
-                return render_template('index.html', posts=posts, pagination=pagination, tags=tags)
+                return render_template('index.html', posts=posts, pagination=pagination, tags=tags, form=form)
         elif request.form['editor1'] == '':
             flash('Please enter content.')
-            return render_template('index.html', posts=posts, pagination=pagination, tags=tags)
+            return render_template('index.html', posts=posts, pagination=pagination, tags=tags, form=form)
         elif recaptcha.verify() == False:
             flash("Please complete reCAPTCHA")
             return render_template('index.html', posts=posts, post_title=request.form['input_title'],
-                                   post_content=request.form['editor1'], pagination=pagination, tags=tags)
+                                   post_content=request.form['editor1'], pagination=pagination, tags=tags, form=form)
         else:
             title = request.form['input_title']
             content = request.form['editor1']
@@ -37,6 +43,11 @@ def index(data=None):
             post = Post(title=title, content=content, is_edited=False, is_visible=True)
             db.session.add(post)
             db.session.commit()
+            for tag in Tag.query.all():
+                if request.form[tag.name] == tag.name:
+                    p_tag = PostTag(post_id=post.id, tag_id=Tag.query.filter_by(name=tag.name).id)
+                    db.session.add(p_tag)
+                    db.session.commit()
             flash('Post submitted!')
             return redirect(url_for('.index'))
-    return render_template('index.html', posts=posts, pagination=pagination, tags=tags)
+    return render_template('index.html', posts=posts, pagination=pagination, tags=tags, form=form)

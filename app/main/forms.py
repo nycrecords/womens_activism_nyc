8b06af6d4e0188c7d92358e1ab53100087379ed3
btang <blt264@nyu.edu>
2016-08-11 12:32:31 -0400

    Changed tag implementation to use multiselect

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,15 +1,7 @@
 from flask_wtf import Form 
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from ..models import Tag
 
 class Comment(Form):
     content = StringField('Comment', validators=[DataRequired("Comment can't be left blank")])
     submit = SubmitField('submit')
-
-
-class TagForm(Form):
-    # for tag in Tag.query.all():
-    #     tag.name = BooleanField(tag.name)
-    tag1 = BooleanField('tag1')
-    tag2 = BooleanField('tag2')
--- app/main/views.py
+++ app/main/views.py
@@ -3,16 +3,11 @@ from .. import db
 from ..models import *
 from . import main
 from .. import recaptcha
-from .forms import TagForm
+# from .forms import TagForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index(data=None):
-
-    form = TagForm()
-
-    tag1 = form.tag1.data
-    print (tag1)
 
     page = request.args.get('page', 1, type=int)
     pagination = Post.query.order_by(Post.creation_time.desc()).paginate(
@@ -23,31 +18,35 @@ def index(data=None):
     tags = Tag.query.all()
 
     if data or request.method == 'POST':
+        print (request.form.getlist('input_tags'))
         if request.form['input_title'] == '':
             if request.form['editor1'] == '':
-                return render_template('index.html', posts=posts, pagination=pagination, tags=tags, form=form)
+                return render_template('index.html', posts=posts, pagination=pagination, tags=tags)
             else:
                 flash('Please enter a title.')
-                return render_template('index.html', posts=posts, pagination=pagination, tags=tags, form=form)
+                return render_template('index.html', posts=posts, pagination=pagination, tags=tags)
         elif request.form['editor1'] == '':
             flash('Please enter content.')
-            return render_template('index.html', posts=posts, pagination=pagination, tags=tags, form=form)
+            return render_template('index.html', posts=posts, pagination=pagination, tags=tags)
+        elif len(request.form.getlist('input_tags')) == 0:
+            flash('Please choose at least one tag.')
+            return render_template('index.html', posts=posts, pagination=pagination, tags=tags)
         elif recaptcha.verify() == False:
             flash("Please complete reCAPTCHA")
             return render_template('index.html', posts=posts, post_title=request.form['input_title'],
-                                   post_content=request.form['editor1'], pagination=pagination, tags=tags, form=form)
+                                   post_content=request.form['editor1'], pagination=pagination, tags=tags)
         else:
             title = request.form['input_title']
             content = request.form['editor1']
-
             post = Post(title=title, content=content, is_edited=False, is_visible=True)
             db.session.add(post)
             db.session.commit()
-            for tag in Tag.query.all():
-                if request.form[tag.name] == tag.name:
-                    p_tag = PostTag(post_id=post.id, tag_id=Tag.query.filter_by(name=tag.name).id)
-                    db.session.add(p_tag)
-                    db.session.commit()
+
+            tag_list = request.form.getlist('input_tags')
+            for tag in tag_list:
+                post_tag = PostTag(post_id=post.id, tag_id=Tag.query.filter_by(name=tag).id)
+                db.session.add(post_tag)
+                db.session.commit()
             flash('Post submitted!')
             return redirect(url_for('.index'))
-    return render_template('index.html', posts=posts, pagination=pagination, tags=tags, form=form)
+    return render_template('index.html', posts=posts, pagination=pagination, tags=tags)

a4d98cf40794b144f14f56746130535749e5e094
Jimmo <jmo@records.nyc.gov>
2016-08-16 13:32:17 -0400

    Merged feature/WOM-24 into develop
    fixed conflicts
    # Conflicts:
    #	app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -19,51 +19,24 @@ app:
 """
 from flask import render_template, redirect, url_for, flash, request, current_app
 from app.db_helpers import put_obj
-from app.models import Post, Tag, PostTag, PostEdit
+from app.models import User, Post, Tag, PostTag
 from app.main import main
 from app import recaptcha
 
 
-@main.route('/simon', methods=['GET', 'POST'])
-# TODO: Delete this route, we don't need it anymore - any changes made by simon needs to be implemented into index.html
-def simonindex(data=None):
-
-    page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.creation_time.desc()).paginate(
-        page, per_page=current_app.config['POSTS_PER_PAGE'],
-        error_out=True)
-    postedit = PostEdit.query.all()
-
-    # need to change this config parameter if I want to change the default 20 posts per page
-
-    posts = pagination.items
-    tags = Tag.query.all()
-    return render_template('index.html', postedit=postedit, posts=posts, pagination=pagination)
-
-
 @main.route('/', methods=['GET', 'POST'])
-def index(data=None):
-    """
-    query the database for a feed of most recent posts
-    query the database for the list of possible tags - passed into drop down selectfield in html
-    :param data: initialized as none because we want a blank form to appear when user first loads page
-    :return: renders template that displays a ckeditor where user can start writing their post.
-        Second half of the page is a feed of most recent posts
-    """
-    # TODO: Update docstring, should not be able to post on index html - instead this should be a new html file in templates/posts/new_story
+# TODO: Delete this route, we don't need it anymore - any changes made by simon needs to be implemented into index.html
+def index():
+    visible_posts = len(Post.query.filter_by(is_visible=True).all())
+
     page = request.args.get('page', 1, type=int)
     pagination = Post.query.order_by(Post.creation_time.desc()).paginate(
         page, per_page=current_app.config['POSTS_PER_PAGE'],
         error_out=True)
-
     posts = pagination.items
-    all_tags = Tag.query.all()
 
     page_posts = []
-    """
-    page_posts is a list of dictionary containing attributes of posts
-    page_posts is used because tags cannot be accessed through posts
-    """
+
     for post in posts:
         post_tags = PostTag.query.filter_by(post_id=post.id).all()
         tags = []
@@ -72,119 +45,31 @@ def index(data=None):
             tags.append(name)
         story = {
             'id': post.id,
-            'title': post.title,
-            'content': post.content,
-            'time': post.creation_time,
+            'activist_first': post.activist_first,
+            'activist_last': post.activist_last,
+            'activist_start': post.activist_start,
+            'activist_end': post.activist_end,
+            'creation_time': post.creation_time,
             'edit_time': post.edit_time,
+            'content': post.content,
             'is_visible': post.is_visible,
             'is_edited': post.is_edited,
             'tags': tags
         }
         page_posts.append(story)
 
-    if data or request.method == 'POST':  # user presses the submit button
-        data = request.form.copy()
-        if data['input_title'] == '':  # user has not entered a title
-            if data['editor1'] == '':  # user has not entered any information into both fields
-                return render_template('index.html', posts=page_posts, post_title=data['input_title'],
-                                       post_content=data['editor1'], pagination=pagination, tags=all_tags)
-            else:
-                flash('Please enter a title.')
-                return render_template('index.html', posts=page_posts, post_title=data['input_title'],
-                                       post_content=data['editor1'], pagination=pagination, tags=all_tags)
-        elif data['editor1'] == '':  # user has not entered a description/content
-            flash('Please enter content.')
-            return render_template('index.html', posts=page_posts, post_title=data['input_title'],
-                                   post_content=data['editor1'], pagination=pagination, tags=all_tags)
-        elif len(request.form.getlist('input_tags')) == 0:
-            flash('Please choose at least one tag.')
-            return render_template('index.html', posts=page_posts, pagination=pagination, tags=all_tags)
-        elif recaptcha.verify() == False:  # user has not passed the recaptcha verification
-            flash("Please complete reCAPTCHA")
-            return render_template('index.html', posts=page_posts, post_title=data['input_title'],
-                                   post_content=data['editor1'], pagination=pagination, tags=all_tags)
-        else:  # successful submission of the post
-            title = data['input_title']
-            content = data['editor1']
+    return render_template('new_index.html', posts=page_posts, pagination=pagination, visible_posts=visible_posts)
 
-            post = Post(title=title, content=content, is_edited=False, is_visible=True)
-            put_obj(post)
 
-            tag_list = request.form.getlist('input_tags')
-            for tag in tag_list:
-                post_tag = PostTag(post_id=post.id, tag_id=Tag.query.filter_by(name=tag).first().id)
-                put_obj(post_tag)
-            flash('Post submitted!')
-            print(page_posts)
-            return redirect(url_for('.index'))
-    return render_template('index.html', posts=page_posts, pagination=pagination, tags=all_tags)
-
-@main.route('/simonabout', methods=['GET', 'POST'])
-def simonabout():
+@main.route('/about', methods=['GET', 'POST'])
+def about():
     # TODO: rename this route
     return render_template('about.html')
 
 
-@main.route('/simonarchive', methods=['GET', 'POST'])
-def simonarchive():
+@main.route('/catalog', methods=['GET', 'POST'])
+def catalog():
     # TODO: rename this route and put it into posts/views.py
-    # TODO: edit archive.html to have the contents of postTab.html and then delete postTab.html
-    return render_template('archive.html')
-
-
-@main.route('/simonshare', methods=['GET', 'POST'])
-def simonshare():
-    # TODO: rename this route and put it into posts/views.py
-    # TODO: update the template so that it has add media button plugin
-    page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.creation_time.desc()).paginate(
-        page, per_page=current_app.config['POSTS_PER_PAGE'],
-        error_out=True)
-    postedit = PostEdit.query.all()
-    posts = pagination.items
-
-    data = request.form.copy()
-    if data or request.method == 'POST':
-        errors = False
-        if data['inspire_name'] == '':
-            flash('Please enter a name.')
-            errors = True
-        if data['input_birth'] == '':
-            flash('Please enter a day of birth.')
-            errors = True
-        if data['input_death'] == '':
-            flash('Please enter a death.')
-            errors = True
-        if data['editor1'] == '':
-            flash('Please share a story.')
-            errors = True
-        if data['input_fullname'] == '':
-            flash('Please enter your full name.')
-            errors = True
-        if data['input_email'] == '':
-            flash('Please enter your email.')
-            errors = True
-        if data['input_website'] == '':
-            flash('Please enter your website.')
-            errors = True
-
-        if errors:
-            return render_template('share.html')
-
-        activist_name = data['inspire_name']
-        activist_start_date = data['input_birth']
-        activist_end_date = data['input_death']
-        content = data['editor1']
-        author_name = data['input_fullname']
-        author_email = data['input_email']
-        author_website = data['input_website']
-        post = Post(activist_name=activist_name, activist_start_date=activist_start_date,
-                    activist_end_date=activist_end_date, author_name=author_name, author_email=author_email,
-                    author_website=author_website, content=content, is_edited=False, is_visible=True)
-        put_obj(post)
-        flash('Post submitted!')
-        return redirect(url_for('.index'))
-    else:
-        return render_template('share.html')
-
-
+    # TODO: edit catalog.html to have the contents of postTab.html and then delete postTab.html
+    tags = Tag.query.all()
+    return render_template('catalog.html', tags=tags)

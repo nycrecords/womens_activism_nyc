8488e5bae5f8261f400b8f09cd7c3cf8a6c7f56e
Jimmo <jmo@records.nyc.gov>
2016-08-18 16:39:17 -0400

    Merged feature/WOM-38 into feature/WOM-99

--- app/main/views.py
+++ app/main/views.py
@@ -23,19 +23,28 @@ from app.models import User, Story, Tag, StoryTag
 from app.main import main
 from sqlalchemy.sql import or_
 import ast
+from app import recaptcha
 
 
 @main.route('/', methods=['GET', 'POST'])
-def index():
+def index(data=None):
+    """
+    query the database for a feed of most recent posts
+    query the database for the list of possible tags - passed into drop down selectfield in html
+    :param data: initialized as none because we want a blank form to appear when user first loads page
+    :return: renders template that displays a ckeditor where user can start writing their post.
+        Second half of the page is a feed of most recent posts
+    """
     visible_stories = len(Story.query.filter_by(is_visible=True).all())
 
     page = request.args.get('page', 1, type=int)
     pagination = Story.query.order_by(Story.creation_time.desc()).paginate(
         page, per_page=current_app.config['STORIES_PER_PAGE'],
         error_out=True)
-    stories = pagination.items
+    stories = pagination.items[:7]
 
     page_stories = []
+
 
     for story in stories:
         story_tags = StoryTag.query.filter_by(story_id=story.id).all()
@@ -43,7 +52,7 @@ def index():
         for story_tag in story_tags:
             name = Tag.query.filter_by(id=story_tag.tag_id).first().name
             tags.append(name)
-        story = {
+        current_story = {
             'id': story.id,
             'activist_first': story.activist_first,
             'activist_last': story.activist_last,
@@ -51,47 +60,92 @@ def index():
             'activist_end': story.activist_end,
             'creation_time': story.creation_time,
             'edit_time': story.edit_time,
-            'content': story.content,
             'is_visible': story.is_visible,
             'is_edited': story.is_edited,
             'tags': tags
         }
-        page_stories.append(story)
+        if story.image_link is not None:
+            current_story['content'] = story.content[:50]
+            current_story['image_link'] = story.image_link
+        else:
+            current_story['content'] = story.content[:150]
+        page_stories.append(current_story)
 
-    return render_template('new_index.html', stories=page_stories, pagination=pagination, visible_stories=visible_stories)
+    missing_stories = 20000 - visible_stories
+
+    first_four = []
+    last_four = []
+    for i in range(0, 7):
+        try:
+            if i <= 3:
+                first_four.append(page_stories[i])
+            else:
+                last_four.append(page_stories[i])
+        except IndexError:
+            break
+
+    stories = []
+    stories.append(first_four)
+    stories.append(last_four)
+
+
+
+    return render_template('index.html', stories=stories, pagination=pagination,
+                           visible_stories=visible_stories, missing_stories=missing_stories)
 
 
 @main.route('/about', methods=['GET', 'POST'])
 def about():
+    # TODO: rename this route
     return render_template('about.html')
 
 
 @main.route('/catalog', methods=['GET', 'POST'])
 def catalog():
     tags = Tag.query.all()
+
     stories = Story.query.all()
+
     return render_template('catalog.html', tags=tags, stories=stories)
 
 
 @main.route('/_get_tags', methods=['GET', 'POST'])
 def get_tags():
     tag_list = request.get_data().decode('utf-8')
+
     tag_list = ast.literal_eval(tag_list)
+
     if len(tag_list) == 0:
+
         stories = Story.query.all()
+
         return render_template('_filtered_stories.html', stories=stories)
+
     else:
+
         clauses = or_(*[StoryTag.tag_id == Tag.query.filter_by(name=tag).first().id for tag in
+
                         tag_list])  # creates filter for query in following line
+
         story_tags = StoryTag.query.filter(clauses).all()  # queries the StoryTag table to find all with above clauses
+
         stories = []
+
         for story_tag in story_tags:  # loops through all story_tags found and appends the related story to the stories list
+
             stories.append(Story.query.filter_by(id=story_tag.story_id).first())
+
         unique_stories = []
+
         stories_dict = {i: stories.count(i) for i in
-                      stories}  # creates a dictionary showing the count that a story shows up for stories list
+
+                        stories}  # creates a dictionary showing the count that a story shows up for stories list
+
         for key, value in stories_dict.items():  # loops through dictionary and appends only the stories that show up
-                                                # the same number of times as the number of tags chosen
+
+            # the same number of times as the number of tags chosen
+
             if stories_dict[key] >= len(tag_list):
                 unique_stories.append(key)
+
         return render_template('_filtered_stories.html', stories=unique_stories)

3b3e91202e6e25c7c009a8db584bea68314dc505
Jimmo <jmo@records.nyc.gov>
2016-08-24 14:48:31 -0400

    merged from feature/WOM-99

--- app/stories/views.py
+++ app/stories/views.py
@@ -86,10 +86,13 @@ def share(data=None):
             valid_video = True
 
         if image_link and image_link != '':
-            if image_link[-3:].lower != 'jpg' or image_link[-3:].lower() != 'png' or image_link[-4:].lower() != 'jpeg':
-                valid_image = False
-            else:
+            if (image_link[-3:].lower() == 'jpg') or (image_link[-3:].lower() == 'png') or (image_link[-4:].lower() == 'jpeg'):
                 valid_image = requests.get(image_link)
+                valid_image = (valid_image.status_code == 200)
+            else:
+                valid_image = False
+        else:
+            valid_image = True
 
         if activist_first_name == '':  # user has not submitted activist first name
             flash("Please enter a first name for women's activist.")

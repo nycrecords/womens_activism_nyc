4c6669c2f2092c79713178f14b2e32608c086076
Jimmo <jmo@records.nyc.gov>
2016-08-24 09:48:57 -0400

    Merged in changes from feature/WOM-99

--- config.py
+++ config.py
@@ -1,48 +1,51 @@
 import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 class Config:
-
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    # SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
-    # MAIL_PORT = os.environ.get('MAIL_PORT') or 2500
-    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or None
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None
-    # MAIL_SUBJECT_PREFIX = os.environ.get('MAIL_SUBJECT_PREFIX') or '[Women\'s Activism NYC]'
-    # MAIL_SENDER = os.environ.get('MAIL_SENDER') or 'Women\'s Activism NYC Admin <womensactivismnyc@gmail.com>'
-    # WOMENS_ADMIN = 'womensactivismnyc@gmail.com'
-    # RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
-    # RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
-    # STORIES_PER_PAGE = 10
-    # COMMENTS_PER_PAGE = 10
-    # RECAPTCHA_ENABLED = True
-    # RECAPTCHA_SITE_KEY = os.environ.get('RECAPTCHA_SITE_KEY')
-    # RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
-
-    # TODO: change hard coding configs to use .env file instead
-    # TODO: add in docstrings
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    MAIL_SERVER = 'smtp.googlemail.com' # TODO: decide on mail server to use: google or amazon
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'womensactivismnyc@gmail.com'
-    MAIL_PASSWORD = 'doris1234'
-    MAIL_SUBJECT_PREFIX = '[Womens Activism NYC]'
-    MAIL_SENDER = 'Womens Activism NYC Admin <womensactivismnyc@gmail.com>'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 2500
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or None
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None
+    MAIL_SUBJECT_PREFIX = os.environ.get('MAIL_SUBJECT_PREFIX') or '[Women\'s Activism NYC]'
+    MAIL_SENDER = os.environ.get('MAIL_SENDER') or 'Women\'s Activism NYC Admin <womensactivismnyc@gmail.com>'
     WOMENS_ADMIN = 'womensactivismnyc@gmail.com'
-    STORIES_PER_PAGE = 10
-
-    RECAPTCHA_PUBLIC_KEY = '6LetUSYTAAAAALgTT8Rt2nLZ2OTBNph6Qa1TbrAH'
-    RECAPTCHA_PRIVATE_KEY = '6LetUSYTAAAAAN1pLPLyyIFUh6rEemXczfMFT4um'
-
+    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
+    STORIES_PER_PAGE = 8
+    COMMENTS_PER_PAGE = 10
     RECAPTCHA_ENABLED = True
-    RECAPTCHA_SITE_KEY = "6LetUSYTAAAAALgTT8Rt2nLZ2OTBNph6Qa1TbrAH"
-    RECAPTCHA_SECRET_KEY = "6LetUSYTAAAAAN1pLPLyyIFUh6rEemXczfMFT4um"
+    RECAPTCHA_SITE_KEY = os.environ.get('RECAPTCHA_SITE_KEY')
+    RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
+
+    # TODO: change hard coding configs to use .env file instead
+    # TODO: add in docstrings
+
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    # SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # MAIL_SERVER = 'smtp.googlemail.com' # TODO: decide on mail server to use: google or amazon
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = 'womensactivismnyc@gmail.com'
+    # MAIL_PASSWORD = 'doris1234'
+    # MAIL_SUBJECT_PREFIX = '[Womens Activism NYC]'
+    # MAIL_SENDER = 'Womens Activism NYC Admin <womensactivismnyc@gmail.com>'
+    # WOMENS_ADMIN = 'womensactivismnyc@gmail.com'
+    # STORIES_PER_PAGE = 10
+    #
+    # RECAPTCHA_PUBLIC_KEY = '6LetUSYTAAAAALgTT8Rt2nLZ2OTBNph6Qa1TbrAH'
+    # RECAPTCHA_PRIVATE_KEY = '6LetUSYTAAAAAN1pLPLyyIFUh6rEemXczfMFT4um'
+    #
+    # RECAPTCHA_ENABLED = True
+    # RECAPTCHA_SITE_KEY = "6LetUSYTAAAAALgTT8Rt2nLZ2OTBNph6Qa1TbrAH"
+    # RECAPTCHA_SECRET_KEY = "6LetUSYTAAAAAN1pLPLyyIFUh6rEemXczfMFT4um"
 
 
 

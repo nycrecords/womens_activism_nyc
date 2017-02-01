4c6b11bb461100f95613a7c8823c91e5709d61e5
Jonathan Yu <jyu@records.nyc.gov>
2017-02-01 16:12:16 -0500

    added unit tests and updated config.py

--- config.py
+++ config.py
@@ -12,17 +12,17 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'postgresql://localhost:5432/womens_activism_nyc_dev2'
+        'postgresql://localhost:5432/womens_activism_nyc_v2_dev'
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'postgresql://localhost:5432/womens_activism_nyc_dev3'
+        'postgresql://localhost:5432/womens_activism_nyc_v2_test'
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://localhost:5432/womens_activism_nyc_dev3'
+        'postgresql://localhost:5432/womens_activism_nyc_v2_prod'
 
 
 config = {

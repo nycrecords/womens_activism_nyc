6d3cccf32d25e9a8b028fdc923e6afa2364909e0
Aman <amanjeet@gmail.com>
2017-02-02 10:42:57 -0500

    Initial files

---
+++ app/main.py
@@ -0,0 +1,12 @@
+#!/Users/Aman/.virtualenvs/womens_activism/bin/python
+from flask import Flask, render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
+if __name__ == '__main__':
+    manager.run()

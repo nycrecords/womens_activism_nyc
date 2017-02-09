a6864ff1f51fcd187dc8a58b61861c109d64a6cd
Aman <amanjeet@gmail.com>
2017-02-09 10:39:28 -0500

    form fixes

--- static/static/js/app.js
+++ static/static/js/app.js
@@ -1,6 +1,15 @@
 window.onload = function() {
+  // Functionality for tags
+  var selectedTags = [];
+
   $(".share-tag" ).click(function() {
     $(this).toggleClass('share-inactive');
     $(this).toggleClass('share-active');
+    if ($.inArray(this.childNodes[0].data, selectedTags) > -1) {
+      selectedTags.splice($.inArray(this.childNodes[0].data, selectedTags), 1);
+    } else {
+      selectedTags.push(this.childNodes[0].data);
+    }
+    console.log(selectedTags);
   });
 }

be80ee7061e2de4f27893d5458fad2246662cbb9
Gary Zhou <gzhou@records.nyc.gov>
2017-02-09 13:18:57 -0500

    Vagrant configurations and scripts

---
+++ build_scripts/proxy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#      ______ ______  ________  ____    __
+#     / __  // __  / / __  /\ \/ /\ \  / /
+#    / /_/ // /_/ / / / / /  \  /  \ \/ /
+#   / ____//   __/ / / / /   /  \   \  /
+#  / /    / /\ \  / /_/ /   / /\ \  / /
+# /_/    /_/  \_\/_____/   /_/  \_\/_/.sh
+#
+
+function set_proxy() {
+	git config --global http.proxy http://bcpxy.nycnet:8080
+	export http_proxy=http://bcpxy.nycnet:8080
+	export https_proxy=http://bcpxy.nycnet:8080
+
+	sudo subscription-manager config --server.proxy_hostname=bcpxy.nycnet --server.proxy_port=8080
+}
+
+function unset_proxy() {
+	git config --global --unset-all http.proxy
+	unset http_proxy
+	unset https_proxy
+	sudo subscription-manager config --server.proxy_hostname= --server.proxy_port=
+}
+
+# Set automatically.
+set_proxy

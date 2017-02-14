a032e073a91f8ce92c04e36aea0e32ee59392300
Aman <amanjeet@gmail.com>
2017-02-14 10:49:41 -0500

    Jinja templating setup

---
+++ new_app/static/js/app.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  console.log('app.js');
+  // Functionality for tags
+  var selectedTags = [];
+
+  $('.share-tag').click(function() {
+    $(this).toggleClass('share-inactive');
+    $(this).toggleClass('share-active');
+    if ($.inArray(this.childNodes[0].data, selectedTags) > -1) {
+      selectedTags.splice($.inArray(this.childNodes[0].data, selectedTags), 1);
+    } else {
+      selectedTags.push(this.childNodes[0].data);
+    }
+    console.log(selectedTags);
+  });
+
+  // Share a story - text counter
+
+  $('#her-story-text').keyup(function() {
+    $('#story-text-count').text($(this).val().length);
+  });
+  $(".nav li").removeClass("active");
+  $('a[href=".' + this.location.pathname + '"]').parents('li,ul').addClass('active');
+}

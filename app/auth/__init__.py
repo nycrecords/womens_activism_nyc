1317008780520b78b1521aadf322e6ac7d8e8631
Aman <amanjeet@gmail.com>
2017-02-14 17:10:43 -0500

    fixed files

--- app/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-auth blueprint
-Contains routes for all User registration and authentication
-"""
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-from app.auth import views
--- app/flags/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-flags blueprint
-Contains the routes for flagging a specific post/comment
-"""
-from flask import Blueprint
-
-flags = Blueprint('flags', __name__)
-
-from app.flags import views

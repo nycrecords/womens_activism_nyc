293177b1ba9a2710e737b0c39d1d62ab6d572b66
Joel Castillo <jcastillo@records.nyc.gov>
2017-02-16 10:42:44 -0500

    Added changes to app setup script

--- build_scripts/app_setup/app_setup.sh
+++ build_scripts/app_setup/app_setup.sh
@@ -23,7 +23,7 @@ sudo yum -y install libxml2-devel xmlsec1-devel xmlsec1-openssl-devel libtool-lt
 source /opt/rh/rh-python35/enable
 pip install virtualenv
 mkdir /home/vagrant/.virtualenvs
-virtualenv /home/vagrant/.virtualenvs/womens_activism
+virtualenv --system-site-packages /home/vagrant/.virtualenvs/womens_activism
 chown -R vagrant:vagrant /home/vagrant
 source /home/vagrant/.virtualenvs/womens_activism/bin/activate
 pip install -r /vagrant/requirements.txt --no-use-wheel

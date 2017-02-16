c98a4af0a3054d188be93562495df511e8d681ad
Aman <amanjeet@gmail.com>
2017-02-16 10:36:19 -0500

    fixes: Code review fixes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -18,6 +18,43 @@ window.onload = function() {
         $('#story-text-count').text($(this).val().length);
     });
 
+    // Share a story - capitalize first letter of name inputs
+    $('#first-name-field').on('keydown', function(event) {
+        if (this.selectionStart == 0 && event.keyCode >= 65 && event.keyCode <= 90 && !(event.shiftKey) && !(event.ctrlKey) && !(event.metaKey) && !(event.altKey)) {
+           var $t = $(this);
+           event.preventDefault();
+           var char = String.fromCharCode(event.keyCode);
+           $t.val(char + $t.val().slice(this.selectionEnd));
+           this.setSelectionRange(1,1);
+        }
+    });
+    $('#user-first-name-field').on('keydown', function(event) {
+        if (this.selectionStart == 0 && event.keyCode >= 65 && event.keyCode <= 90 && !(event.shiftKey) && !(event.ctrlKey) && !(event.metaKey) && !(event.altKey)) {
+           var $t = $(this);
+           event.preventDefault();
+           var char = String.fromCharCode(event.keyCode);
+           $t.val(char + $t.val().slice(this.selectionEnd));
+           this.setSelectionRange(1,1);
+        }
+    });
+    $('#user-last-name-field').on('keydown', function(event) {
+        if (this.selectionStart == 0 && event.keyCode >= 65 && event.keyCode <= 90 && !(event.shiftKey) && !(event.ctrlKey) && !(event.metaKey) && !(event.altKey)) {
+           var $t = $(this);
+           event.preventDefault();
+           var char = String.fromCharCode(event.keyCode);
+           $t.val(char + $t.val().slice(this.selectionEnd));
+           this.setSelectionRange(1,1);
+        }
+    });
+    $('#last-name-field').on('keydown', function(event) {
+        if (this.selectionStart == 0 && event.keyCode >= 65 && event.keyCode <= 90 && !(event.shiftKey) && !(event.ctrlKey) && !(event.metaKey) && !(event.altKey)) {
+           var $t = $(this);
+           event.preventDefault();
+           var char = String.fromCharCode(event.keyCode);
+           $t.val(char + $t.val().slice(this.selectionEnd));
+           this.setSelectionRange(1,1);
+        }
+    });
     //Nav active state change
     $(".nav li").removeClass("active");
     $('a[href=".' + this.location.pathname + '"]').parents('li,ul').addClass('active');

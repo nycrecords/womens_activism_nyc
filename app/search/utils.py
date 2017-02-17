51ff5758416fea6a80ce399cce295ec286940630
Gary Zhou <gzhou@records.nyc.gov>
2017-02-16 19:21:36 -0500

    Working on creating dsl

--- app/search/utils.py
+++ app/search/utils.py
@@ -68,6 +68,8 @@ def search_stories(query,
                    activist_last,
                    content,
                    tag,
+                   size,
+                   start,
                    by_phrase=False,
                    highlight=False):
     """
@@ -79,6 +81,8 @@ def search_stories(query,
     :param activist_last: search by activist's last name?
     :param content: search by story content?
     :param tag: search by tag
+    :param size: number of stories
+    :param start: starting index of story result set
     :param by_phrase: use phrase matching instead of full-text?
     :param highlight: return highlights?
         if True, will come at a slgiht performance cost (in order to
@@ -94,6 +98,8 @@ def search_stories(query,
     if query and not any((activist_first, activist_last, content, tag)):
         return MOCK_EMPTY_ELASTICSEARCH_RESULT
 
+    # TODO: tags from search
+
     # set matching type (full-text or phrase matching)
     match_type = 'match_phrase' if by_phrase else 'match'
 
@@ -105,14 +111,72 @@ def search_stories(query,
         'tag': tag
     }
     dsl_gen = StoriesDSLGenerator(query, query_fields, match_type)
+    dsl = dsl_gen.search() if query else dsl_gen.queryless()
+
+    # search/run query
+    results = es.search(
+        index=current_app.config["ELASTICSEARCH_INDEX"],
+        doc_type='story',
+        body=dsl,
+        _source=['activist_first',
+                 'activist_last',
+                 'content',
+                 'tag'],
+        size=size,
+        from_=start,
+    )
+
+    return results
 
 
 class StoriesDSLGenerator(object):
     """
     Class for generating dicts representing query dsl bodies for searching story docs.
     """
-    def __init__(self, query, query_fields, match_type):
+    def __init__(self, query, query_fields, tags, match_type):
         self.__query = query
         self.__query_fields = query_fields
         self.__match_type = match_type
+
+        self.__default_filters = [{'terms': {'tags': tags}}]
+        self.__filters = []
+        self.__conditions = []
+
+    def search(self):
+        for name, use in self.__query_fields.items():
+            if use:
+                self.__filters = [
+                    {self.__match_type: {
+                        name: self.__query
+                    }}
+                ]
+                self.__conditions.append(self.__must)
+        return self.__should
+
+    def queryless(self):
+        self.__filters = [
+            {'match_all': {}}
+        ]
+        return self.__must
+
+    @property
+    def __must(self):
+        return {
+            'bool': {
+                'must': self.__get_filters()
+            }
+        }
+
+    @property
+    def __should(self):
+        return {
+            'query': {
+                'bool': {
+                    'should': self.__conditions
+                }
+            }
+        }
+
+    def __get_filters(self):
+        return self.__filters + self.__default_filters
 

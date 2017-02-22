12830d8009a11642ce369e2bbf2a64ccfc036092
Aman <amanjeet@gmail.com>
2017-02-22 09:34:26 -0500

    Initial layout

--- app/story/views.py
+++ app/story/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 @story.route('/', methods=['GET', 'POST'])
 def share():
     return render_template('story/share.html')
+
+@story.route('/view', methods=['GET', 'POST'])
+def view():
+    return render_template('story/view.html')

4046d1cbfb0a2011dd44c2405f8a920ebce04316
Jonathan Yu <jyu@records.nyc.gov>
2017-02-24 12:36:38 -0500

    added dictionary object to preserve form data in case there is an error

--- app/share/views.py
+++ app/share/views.py
@@ -11,6 +11,19 @@ def new():
     :return: If the story form was fully validated, create a Story and Poster object to store in the database
     """
     form = StoryForm()
+    story = {
+        "activist_first": form.activist_first.data,
+        "activist_last": form.activist_last.data,
+        "activist_start": form.activist_start.data,
+        "activist_end": form.activist_end.data,
+        "content": form.content.data,
+        "activist_url": form.activist_url.data,
+        "image_url": form.image_url.data,
+        "video_url": form.video_url.data,
+        "poster_first": form.poster_first.data,
+        "poster_last": form.poster_first.data,
+        "poster_email": form.poster_email.data
+    }
     if request.method == 'POST':
         tag_string = form.tags.data
         tags = tag_string.split(';')
@@ -22,13 +35,13 @@ def new():
                               form.activist_end.data,
                               form.activist_end_BC.data):
                 flash('Error, please try again.')
-                return render_template('stories/share_a_story.html', form=form)
+                return render_template('stories/share_a_story.html', form=form, story=story)
 
             if form.poster_first.data or form.poster_last.data or form.poster_email.data:
                 # extra validator for poster information because it can't be done through WTForms built in
                 if not validate_poster(form.poster_first.data, form.poster_last.data):
                     flash('Error, please try again.')
-                    return render_template('stories/share_a_story.html', form=form)
+                    return render_template('stories/share_a_story.html', form=form, story=story)
                 poster_id = create_poster(poster_first=form.poster_first.data,
                                           poster_last=form.poster_last.data,
                                           poster_email=form.poster_email.data)
@@ -50,4 +63,4 @@ def new():
             flash('Story submitted!', category='share_submitted_story')
             return redirect(url_for('share.new'))
         flash('Error, please try again.')
-    return render_template('share/share.html', form=form)
+    return render_template('share/share.html', form=form, story=story)

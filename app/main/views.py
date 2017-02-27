9d38db702ff71ba31a97a87fc1943cbf3cacdf0d
Aman <amanjeet@gmail.com>
2017-02-27 11:00:54 -0500

    Contact Page

--- app/main/views.py
+++ app/main/views.py
@@ -10,3 +10,7 @@ def index():
 @main.route('/about', methods=['GET'])
 def about():
     return render_template('main/about.html')
+
+@main.route('/contact', methods=['GET'])
+def contact():
+    return render_template('main/contact.html')

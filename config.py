5238953510dd2b221c5f61de6b0bf59a71349ffd
Gary Zhou <gzhou@records.nyc.gov>
2017-02-28 17:57:36 -0500

    Updated Config

--- config.py
+++ config.py
@@ -10,9 +10,21 @@ load_dotenv(dotenv_path)
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
     # Flask-SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # remove once this becomes the default
 
+    # Elasticsearch settings
+    ELASTICSEARCH_HOST = os.environ.get('ELASTICSEARCH_HOST') or 'localhost:9200'
+    ELASTICSEARCH_ENABLED = os.environ.get('ELASTICSEARCH_ENABLED') == "True"
+    ELASTICSEARCH_INDEX = os.environ.get('ELASTICSEARCH_INDEX') or 'stories'
+    ELASTICSEARCH_USE_SSL = os.environ.get('ELASTICSEARCH_USE_SSL') == 'True'
+    ELASTICSEARCH_USERNAME = os.environ.get('ELASTICSEARCH_USERNAME')
+    ELASTICSEARCH_PASSWORD = os.environ.get('ELASTICSEARCH_PASSWORD')
+    ELASTICSEARCH_HTTP_AUTH = ((ELASTICSEARCH_USERNAME,
+                                ELASTICSEARCH_PASSWORD)
+                               if ELASTICSEARCH_USERNAME and ELASTICSEARCH_PASSWORD
+                               else None)
 
     @staticmethod
     def init_app(app):
@@ -45,7 +57,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
-                               'postgresql://localhost:5432/womens_activism_prod')
+                               'postgresql://localhost:5432/womens_activism_nyc_v2')
 
 
 config = {

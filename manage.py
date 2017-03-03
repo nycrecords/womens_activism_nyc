0049898f58d55ce9ac2a8095a4d0f53b14acff03
Jonathan Yu <jyu@records.nyc.gov>
2017-03-03 16:02:11 -0500

    created manager command

--- manage.py
+++ manage.py
@@ -13,6 +13,9 @@ from app.models import (
 )
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+import csv
+from app.constants import module
+from app.db_utils import create_object
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -49,6 +52,42 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
+@manager.option("-f", "--featured", help="Create featured story module.", action="store_true", dest='featured')
+def modules(featured=False):
+    if featured:
+        with open('csv/featured.csv', 'r') as csvfile:
+            featured_csv = csv.reader(csvfile, delimiter= ';')
+            next(featured_csv)  # skip the header
+            row = next(featured_csv)
+
+            story_id = row[0]
+            quote = row[1]
+
+            # disables the current featured story
+            current_featured = Modules.query.filter_by(type=module.FEATURED, is_active=True).one()
+            current_featured.is_active = False
+            db.session.commit()
+
+            featured_story = Stories.query.filter_by(id=story_id).one()
+            activist_first = featured_story.activist_first
+            activist_last = featured_story.activist_last
+            media_url = featured_story.image_url
+
+            featured_module = Modules(story_id=story_id,
+                                      type=module.FEATURED,
+                                      title1=None,
+                                      title2=None,
+                                      activist_first=activist_first,
+                                      activist_last=activist_last,
+                                      content=quote,
+                                      media_url=media_url,
+                                      event_date=None,
+                                      activist_year=None,
+                                      is_active=True)
+            create_object(featured_module)
+            print("New featured story module set")
+        csvfile.close()
 
 if __name__ == '__main__':
     manager.run()

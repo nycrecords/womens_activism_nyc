4589efd4e9074e1d4b6498aeccde6bdb013fbcba
Joseph Park <heeseung5@gmail.com>
2018-02-20 22:16:41 -0500

    Remove remember me data and fix url

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,7 +10,7 @@ def login():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
+            login_user(user)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
 
@@ -21,4 +21,4 @@ def login():
 def logout():
     logout_user()
     flash("You have been logged out.")
-    return redirect(url_for('main_index'))
+    return redirect(url_for('main.index'))

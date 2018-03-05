4e5c440ec21e1faee74a525131b457cdf3b46789
Joseph Park <heeseung5@gmail.com>
2018-03-05 18:05:42 -0500

    Add audit trail for event for admin login

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -2,11 +2,24 @@
 Utility functions used for view functions involving login
 '''
 
-from app.models import Users
+from app.models import Users, Events
+from app.db_utils import create_object
+from app.constants.event import LOGIN_FAILED, LOGIN_SUCCESS
 
-def create_login_log(form_email, login_validation):
+def create_login_log(user, login_validation):
     '''
     A utility function to log login success and failure in Events table
     :param form.data
     '''
-    user = Users.query.filter_by(email=form_email).first()
+    if login_validation:
+        if user is not None:
+            create_object(Events(
+                _type=LOGIN_SUCCESS,
+                user_guid=user.guid
+            ))
+    else:
+        tempGuid = None if user is None else user.guid
+        create_object(Events(
+            _type=LOGIN_FAILED,
+            user_guid=tempGuid
+        ))
--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from app.models import Users
 from .forms import LoginForm
-from app.utils import create_login_log
+from app.auth.utils import create_login_log
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -12,14 +12,14 @@ def login():
 
     form = LoginForm()
     if form.validate_on_submit():
-        user = Users.query.filter_by(email=form.email.data).first()
+        user = Users.query.filter_by(email=form.email.data, is_admin=True).one_or_none()
         if user is not None and user.verify_password(form.password.data) and user.is_admin:
             login_user(user)
-            create_login_log(form.email.data, True)
+            create_login_log(user, True)
             return redirect(request.args.get('next') or url_for('main.index'))
 
         flash('Invalid username or password.')
-        create_login_log(form.email.data, False)
+        create_login_log(user, False)
 
     return render_template('auth/login.html', form=form)
 

6992fe8b9a6feb68c914fd98b9614d940e3723c2
Joseph Park <heeseung5@gmail.com>
2018-03-07 17:10:06 -0500

    Comply quotation marks rule

--- app/stories/views.py
+++ app/stories/views.py
@@ -3,9 +3,10 @@ View functions for story functionality
 """
 from app.stories import stories
 from app.edit.utils import hide_story
-from app.edit.forms import HideForm
+from app.feature.utils import remove_featuredstory
 from flask import render_template, abort, request, flash, redirect, url_for
 from sqlalchemy.orm.exc import NoResultFound
+from app.edit.forms import HideForm
 
 from app.constants.video_url import (
     YOUTUBE_FULL_URL,
@@ -16,7 +17,7 @@ from app.constants.video_url import (
     VIMEO_URL,
     VIMEO_EMBED_URL
 )
-from app.models import Stories, Tags, Users
+from app.models import Stories, Tags, Users, FeaturedStories
 
 
 @stories.route('/catalog/', methods=['GET'])
@@ -35,9 +36,13 @@ def view(story_id):
     form = HideForm(request.form)
 
     if request.method == 'POST':
-        if form.validate_on_submit():
+        if request.form['submit'] == "Hide this Story":
             hide_story(story_id)
-            flash('Story Hidden!', category='success')
+            flash("Story Hidden!", category='success')
+            return redirect(url_for('stories.catalog'))
+        elif request.form['submit'] == "Remove this Featured Story":
+            remove_featuredstory(story_id)
+            flash("This story is now hidden from the Featured Stories")
             return redirect(url_for('stories.catalog'))
 
     try:
@@ -49,9 +54,9 @@ def view(story_id):
     except AssertionError:
         print("Story is not visible")
         return abort(404)
-
     if story.is_visible:
         user = Users.query.filter_by(guid=story.user_guid).one() if story.user_guid else None
+        feature = FeaturedStories.query.filter_by(story_id=story.id).one_or_none()
 
         video_url = None
         if story.video_url:
@@ -65,5 +70,5 @@ def view(story_id):
             elif VIMEO_STRING in video_url:
                 split = video_url.split(VIMEO_URL, 1)
                 video_url = VIMEO_EMBED_URL.format(split[1])
-        return render_template('stories/view.html', story=story, user=user, video_url=video_url, form=form)
-
+        return render_template('stories/view.html', story=story, user=user, video_url=video_url,
+                               feature=feature, form=form)

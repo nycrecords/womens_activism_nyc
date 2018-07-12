cf5d897dcd20f5f6b1f2120054179861272ae8e8
Joel Castillo <jocastillo@records.nyc.gov>
2018-07-12 14:03:00 -0400

    Adding .env.example

--- manage.py
+++ manage.py
@@ -25,27 +25,29 @@ from app.db_utils import create_object
 from app.constants.event import EDIT_FEATURED_STORY
 from sqlalchemy.orm.exc import NoResultFound
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv("FLASK_CONFIG") or "default")
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app,
-                db=db,
-                Roles=Roles,
-                Users=Users,
-                Stories=Stories,
-                Tags=Tags,
-                Comments=Comments,
-                Events=Events,
-                Modules=Modules,
-                Flags=Flags,
-                Feedback=Feedback)
+    return dict(
+        app=app,
+        db=db,
+        Roles=Roles,
+        Users=Users,
+        Stories=Stories,
+        Tags=Tags,
+        Comments=Comments,
+        Events=Events,
+        Modules=Modules,
+        Flags=Flags,
+        Feedback=Feedback,
+    )
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
+manager.add_command("db", MigrateCommand)
 
 
 @manager.command
@@ -60,10 +62,7 @@ def create_user():
     if password != confirm_password:
         return print("Passwords are not the same. Please try again.")
 
-    user = Users(str(uuid.uuid4()),
-                 AGENCY_USER,
-                 email=email,
-                 is_admin=True)
+    user = Users(str(uuid.uuid4()), AGENCY_USER, email=email, is_admin=True)
 
     user.set_password(password)
 
@@ -91,6 +90,7 @@ def deploy():
 def es_recreate():
     """Recreate elasticsearch index and request docs."""
     from app.search.utils import recreate
+
     recreate()
 
 
@@ -98,11 +98,18 @@ def es_recreate():
 def test():
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+
+    tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-@manager.option("-f", "--featured", help="Create featured story module.", action="store_true", dest='featured')
+@manager.option(
+    "-f",
+    "--featured",
+    help="Create featured story module.",
+    action="store_true",
+    dest="featured",
+)
 def modules(featured=False):
     """
     Manage function modules that inserts a single featured story entry to the modules table.
@@ -111,8 +118,8 @@ def modules(featured=False):
     """
     if featured:
         # open csv file
-        with open(current_app.config['FEATURED_DATA'], 'r') as csvfile:
-            featured_csv = csv.reader(csvfile, delimiter=';')
+        with open(current_app.config["FEATURED_DATA"], "r") as csvfile:
+            featured_csv = csv.reader(csvfile, delimiter=";")
             next(featured_csv)  # skip the header
             row = next(featured_csv)
 
@@ -121,7 +128,9 @@ def modules(featured=False):
 
             # disable current featured story
             try:
-                current_featured = Modules.query.filter(Modules.type == FEATURED, Modules.is_active == True).one()
+                current_featured = Modules.query.filter(
+                    Modules.type == FEATURED, Modules.is_active == True
+                ).one()
             except NoResultFound:
                 current_featured = None
                 print("No featured module set")
@@ -133,25 +142,31 @@ def modules(featured=False):
                 db.session.commit()
 
             # create and store object into Modules table
-            new_featured_story = Stories.query.filter(Stories.id == story_id, Stories.is_visible == True).one()
-            featured_module = Modules(story_id=story_id,
-                                      type=FEATURED,
-                                      activist_first=new_featured_story.activist_first,
-                                      activist_last=new_featured_story.activist_last,
-                                      content=quote,
-                                      media_url=new_featured_story.image_url,
-                                      is_active=True)
+            new_featured_story = Stories.query.filter(
+                Stories.id == story_id, Stories.is_visible == True
+            ).one()
+            featured_module = Modules(
+                story_id=story_id,
+                type=FEATURED,
+                activist_first=new_featured_story.activist_first,
+                activist_last=new_featured_story.activist_last,
+                content=quote,
+                media_url=new_featured_story.image_url,
+                is_active=True,
+            )
             create_object(featured_module)
 
             # create and store Events object
-            edit_featured_event = Events(_type=EDIT_FEATURED_STORY,
-                                         module_id=featured_module.id,
-                                         previous_value=previous_value,
-                                         new_value=featured_module.val_for_events)
+            edit_featured_event = Events(
+                _type=EDIT_FEATURED_STORY,
+                module_id=featured_module.id,
+                previous_value=previous_value,
+                new_value=featured_module.val_for_events,
+            )
             create_object(edit_featured_event)
             print("New featured story module set")
         csvfile.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()

fd128fe4b39d62438bfdd0452c4a05113e3a2d56
Tracey Kong <tkong@records.nyc.gov>
2018-07-26 12:26:24 -0400

    added modify.js for feature story form pages

---
+++ app/static/js/feature/modify.js
@@ -0,0 +1,24 @@
+$(function() {
+    var titleInput = $("#title");
+    var descriptionInput = $("#description");
+
+    var requiredFields = ["title", "description"];
+    for (var i = 0; i < requiredFields.length; i++) {
+        $("#" + requiredFields[i]).attr("data-parsley-required","");
+    }
+
+    titleInput.attr("data-parsley-maxlength", 90);
+    descriptionInput.attr("data-parsley-maxlength", 395);
+
+    // var errorList = [];
+    // window.Parsley.on('field:error', function () {
+    //     if (!errorList[0]) {
+    //         errorList.push(this.$element);
+    //         $("html, body").animate({
+    //             scrollTop: errorList[0].parent().parent().parent().find('h1').offset().top
+    //         }, 1000, function () {
+    //             errorList = [];
+    //         });
+    //     }
+    // });
+})

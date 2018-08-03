3c86f2e06b23906e4c8f5ca714285a0b732c9f65
Eileen Asinc <easinc@records.nyc.gov>
2018-08-03 14:52:01 -0400

    worked on adding a csv file when the admin presses submit

--- app/export/views.py
+++ app/export/views.py
@@ -1,10 +1,9 @@
-from flask import render_template, request, flash, redirect, url_for,Markup
+from flask import render_template, request, flash, redirect, url_for, Markup, current_app
 from flask_login import login_required
 from app.export import export
 from app.export.forms import ExportForm
-import io
 import csv
-from flask import make_response
+from io import StringIO, BytesIO
 from app.models import Users
 
 
@@ -14,17 +13,26 @@ def export():
     form = ExportForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
-            user = Users.query.filter_by(first_name=form.user_first.data,
-                                         last_name=form.user_last.data,
-                                         email=form.user_email.data,
-                                         phone=form.user_phone.data,
-                                         subscription=True).one_or_none()
-            si = io.BytesIO()
-            cw = csv.writer(si, dialect='excel')
-            cw.writerows(user)
-            output = make_response(si.getvalue())
-            output.headers["Content-Disposition"] = "attachment; filename=export.csv"
-            output.headers["Content-type"] = "text/csv"
-            flash(Markup('Now exporting!'), category='success')
-            return output
+            users = Users.query.filter_by(subscription=True).all()
+            if users:
+                buffer = StringIO()  # csvwriter cannot accept BytesIO
+                writer = csv.writer(buffer)
+                writer.writerow(["First Name",
+                                 "Last Name",
+                                 "Email",
+                                 "Phone Number"])
+                writer.writerow([users.first_name or '',
+                                 users.last_name or '',
+                                 users.email or '',
+                                 users.phone or ''])
+
+                return export(
+                    BytesIO(buffer.getvalue().encode('UTF-8')),  # convert to bytes
+                    attachment_filename="subscribers.csv",
+                    as_attachment=True
+                )
+            return '', 400
+            
+            #flash(Markup('Now exporting!'), category='success')
+
     return render_template('export/export.html', form=form)

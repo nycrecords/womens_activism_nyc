91b18bc23cb4daed76940fd8146fee62e65c20d3
Tracey Kong <tkong@records.nyc.gov>
2018-08-10 15:01:36 -0400

    styled twitter

--- app/static/js/plugins/twitter.js
+++ app/static/js/plugins/twitter.js
@@ -1,23 +1,55 @@
-$('#rightArrow').click(function() {
-    $('#tweetsRow').animate({
-        scrollLeft:"+="+380+"px"
-    }, 'slow', function() {
+$(function () {
+    $(window).bind("load", function () {
+        var tweets = $('#tweetsRow li').toArray();
+        var twitter = $('#tweetsRow ul');
+        var newHeight;
+        var containerHeight = $('#twitter .container').outerHeight();
+        $.each(tweets, function () {
+            newHeight = twitter.outerHeight() - $(this).find(".user").outerHeight() -
+                                                $(this).find(".tweet").outerHeight() -
+                                                $(this).find(".interact").outerHeight(true) -
+                                                $(this).find(".timePosted").outerHeight(true) - 40;
+            if($(this).find(".media img").height() > $(this).find(".media img").width()) {
+                $(this).find(".media img").css({
+                    "height": newHeight*1.5 + "px"
+                });
+            } else {
+                $(this).find(".media img").css({
+                    "height": newHeight + "px"
+                });
+            }
+            $(this).find(".media").css({
+                "height": newHeight + "px"
+            });
+        });
 
-        $('html, body').animate({
-            scrollLeft: 0
-        }, 800);
+        $('#leftArrow').css('height',containerHeight);
+        $('#rightArrow').css('height',containerHeight);
 
     });
-});
 
-$('#leftArrow').click(function() {
-    $('#tweetsRow').animate({
-        scrollLeft:"-="+380+"px"
-    }, 800, function() {
+    $('#rightArrow').click(function() {
+        $('#tweetsRow ul').animate({
+            scrollLeft:"+="+380+"px"
+        }, 700, function() {
 
-        $('html, body').animate({
-            scrollLeft: 0
-        }, 800);
+            $('html, body').animate({
+                scrollLeft: 0
+            }, 700);
 
+        });
     });
+
+    $('#leftArrow').click(function() {
+        $('#tweetsRow ul').animate({
+            scrollLeft:"-="+380+"px"
+        }, 700, function() {
+
+            $('html, body').animate({
+                scrollLeft: 0
+            }, 700);
+
+        });
+    });
+
 });

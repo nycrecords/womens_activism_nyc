26d84ff7df377096a12df3762b7f796cce1115cb
Tracey Kong <tkong@records.nyc.gov>
2018-08-16 14:40:54 -0400

    added edit, remove and add features

--- app/static/js/feature/tag.js
+++ app/static/js/feature/tag.js
@@ -1,44 +1,78 @@
 $(function () {
     var tagRows = $('tr').toArray();
-    var updateButtons = $('.tag-update').toArray();
+    var adding = false;
+
+    $('#tag-add').parent().parent().hide();
 
     $.each(tagRows, function () {
 
         $(this).find('.tag-name').hide();
-        $(this).find('.tag-update').hide();
+        $(this).find('#tag-update').hide();
 
-        $(this).find('#edit').click(function () {
-            $(this).parent().parent().find('#edit').toggle();
-            $(this).parent().parent().find('#name').toggle();
+        $(this).find('#tag-edit').click(function () {
+            $(this).parent().parent().find('#tag-edit').toggle();
+            $(this).parent().parent().find('#tag-remove').toggle();
+            $(this).parent().parent().find('#tag-name').toggle();
+            $(this).parent().parent().find('#tag-update').toggle();
             $(this).parent().parent().find('.tag-name').toggle();
-            $(this).parent().parent().find('.tag-update').toggle();
         });
-    });
 
-    var successmessage;
-
-    $.each(updateButtons, function () {
-        $(this).click(function () {
-            var id = $(this).attr("id");
+        $(this).find('#tag-update').click(function () {
+            var id = $(this).attr("value");
             var name = $("#tag-name-" + id).val();
 
-            $.ajax({
-                url: "/tag/update",
-                type: "POST",
-                data: {
-                    id: id,
-                    name: name
-                },
-                success: function (data) {
-                    successmessage = 'success';
-                }
-            });
+            getAjax(id, name, 'edit');
 
-            $(this).parent().parent().find('#name').text(name);
-            $(this).parent().parent().find('#edit').toggle();
-            $(this).parent().parent().find('#name').toggle();
+            $(this).parent().parent().find('#tag-name').text(name);
+            $(this).parent().parent().find('#tag-edit').toggle();
+            $(this).parent().parent().find('#tag-name').toggle();
+            $(this).parent().parent().find('#tag-remove').toggle();
             $(this).parent().parent().find('.tag-name').toggle();
             $(this).toggle();
         });
+
+        $(this).find('#tag-remove').click(function () {
+            var id = $(this).attr("value");
+            getAjax(id, null, 'remove');
+
+            $(this).parent().parent().fadeOut(1000);
+        });
+    });
+    
+    $('.tag-add').click(function () {
+        if(!adding){
+            $('#tag-add').parent().parent().toggle();
+            adding = true;
+        }
+    });
+
+    $('#tag-add').click(function () {
+        adding = false;
+        var new_name = $('#tag-name-add').val();
+        getAjax(null, new_name, 'add');
+        $('#tag-name-add').val(' ');
+        $('#tag-add').parent().parent().toggle();
+    })
+
+    $('#tag-cancel').click(function () {
+        adding = false;
+        $('#tag-name-add').val(' ');
+        $('#tag-add').parent().parent().toggle();
     });
 });
+
+function getAjax(id, name, action){
+    $.ajax({
+        url: "/tag/update",
+        type: "POST",
+        data: {
+            id: id,
+            name: name,
+            action: action
+        },
+        success: function() {
+            window.location.reload();
+            $(window).scrollTop(0);
+        }
+    });
+}
--- app/tag/views.py
+++ app/tag/views.py
@@ -1,26 +1,42 @@
 """
 View functions for story functionality
 """
-from flask import render_template, request, jsonify
+from flask import render_template, request, flash, jsonify
 from flask_login import login_required
 
+from app import db
 from app.models import Tags
 from app.tag import tag
+from app.db_utils import create_object, update_object
+from operator import attrgetter
 
 
 @tag.route('/edit_tags', methods=['GET', 'POST'])
 @login_required
 def edit_tags():
-    tags = Tags.query.all()
+    tags = sorted(Tags.query.all(), key=attrgetter('id'))
 
     return render_template('tag/edit_tags.html', tags=tags)
 
 
-@tag.route('/update', methods=['POST'])
+@tag.route('/update', methods=['GET', 'POST'])
 @login_required
 def update():
-        print('hello')
-        tag_id = request.form['id']
-        name = request.form['name']
-        print(tag_id, name)
-        return jsonify({'result': 'success'})
+    tag_id = request.form['id']
+    name = request.form['name']
+    action = request.form['action']
+
+    if action == "edit":
+        update_object({'name': name}, Tags, tag_id)
+        flash('Tag is edited successfully!', category='success')
+    elif action == "remove":
+        tag = Tags.query.get(tag_id)
+        db.session.delete(tag)
+        db.session.commit()
+        flash('Tag is removed successfully!', category='success')
+    else:
+        new_name = request.form['name']
+        create_object(Tags(name=new_name))
+        flash('Tag is added successfully!', category='success')
+
+    return jsonify({'result': 'success'})
